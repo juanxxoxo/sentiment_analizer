aaa3e4b2de1df688202e3444ea62a88a68d2526e
Juanmitaa <112973440+Juanmitaa@users.noreply.github.com>
2023-04-24 18:24:02 -0400

    first commit

---
+++ sentiment_analyzer.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+def get_sentiment(text):
+    sid = SentimentIntensityAnalyzer()
+    scores = sid.polarity_scores(text)
+    if scores['compound'] > 0.05:
+        return 'positive'
+    elif scores['compound'] < -0.05:
+        return 'negative'
+    else:
+        return 'neutral'
+
+
+text = "Charles enjoys playing football but Marie does not"
+sentiment = get_sentiment(text)
+print("Sentiment:", sentiment)
